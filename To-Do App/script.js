a400c14b1d4ce2a51f3d6ce33d219e41b939e2a7
Satyavati05 <satyayougni123@gmail.com>
2026-07-01 15:11:50 +0530

    Initial commit

---
+++ To-Do App/script.js
@@ -0,0 +1,14 @@
+function addTask(){
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+    if (taskText === "") return;
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    li.addEventListener("click", ()=> {
+        li.remove();
+    }
+
+    );
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
